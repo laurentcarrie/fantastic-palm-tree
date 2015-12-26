f3712d8cf596bb230cf9f287c0d4ac254bfdf37a
laurent <laurent.carrie@gmail.com>
2015-12-26 20:47:01 +0100

    ok

--- go.sh
+++ go.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 
+set -e
+set -x 
+
 rm -f song-to-html
-ocamlfind ocamlopt -o song-to-html read.ml -linkpkg -package extlib
+ocamlfind ocamlopt -warn-error all  -o song-to-html read.ml -linkpkg -package extlib
+
+songs=perfect-day
+
+for s in $songs ; do
+    ./song-to-html $s.song $s.html
+done
--- read.ml
+++ read.ml
@@ -1,61 +1,92 @@
 open ExtString
 open Printf
 
+
 type context =
     | Normal
-    | Grille of string
-(*
-module String = struct
-  include String
-  let split s sep = 
-    try
-      split s sep
-    with
-      | Invalid_string -> failwith("invalid string : '" ^ s ^ "'")
-end
-*)
-let read filename = 
-  let fin = open_in filename in
-  let rec r acc context =
+    | Titre of string
+    | Grille of string list
+
+type document = context list 
+
+
+let print_title (pf : ('a, unit, string, unit) format4 -> 'a) title = (
+  pf "<h1>%s</h1>" title
+) ;;
+
+let read_array_until_empty_line fin = (
+  let rec r acc =
     try
       let line = input_line fin in
-      let line = 
-	try 
-	  let (command,line2) = String.split line " " in
-	    match command with
-	      | "\\title" 
-	      | "\\titre" -> sprintf "<h1>%s</h1>" line2
-	      | _ -> line
-	with 
-	  | Invalid_string -> line
-	in
-	r (line::acc) context
+      let line = String.strip line in
+      if line = "" then ( List.rev acc) else ( r (line::acc))
     with
-      | End_of_file -> (
-	  close_in fin ;
-	  List.rev acc
-	)
+    | End_of_file -> (List.rev acc)
   in
+  r []
+) ;;
 
-  let data = r [] Normal in
-    data
+let read_string_until_empty_line fin = (
+  let a = read_array_until_empty_line fin in
+  String.join "\n" a
+) ;;
 
+let print_grille  (pf : ('a, unit, string, unit) format4 -> 'a) (g:string list) = (
+  pf "%s" "\n<table>\n" ;
+  List.iter ( fun line ->
+    pf "%s" "<tr>"  ;
+    let a = String.nsplit line ":" in
+    List.iter ( fun a -> pf "<td>%s</td>" a ) a ;
+    pf "%s" "</tr>\n"  ;
+  ) g ;
+  pf "%s" "</table>\n" 
+) ;;
+
+let read filename : document = 
+  let fin = open_in filename in
+  let rec r acc  = 
+    try
+      let line = String.strip (input_line fin) in
+      match line with
+      | "\\titre" -> r ((Titre (read_string_until_empty_line fin))::acc)
+      | "\\grille" -> r ((Grille (read_array_until_empty_line fin))::acc)
+      | s -> failwith ("bad string : '" ^ s ^ "'")
+    with
+    | End_of_file -> (
+      close_in fin ;
+      List.rev acc
+    )
+  in
+  let data = r []  in
+  data
+    
 
 let _ =
   try
     let data = read Sys.argv.(1) in
-    let en_tete = "<!DOCTYPE html>
+    let title = List.fold_left ( fun acc d -> match d with | Titre s -> s | _ -> acc ) "" data in
+    let fout = open_out (Sys.argv.(2)) in
+    let pf fs = ksprintf ( fun s -> fprintf fout "%s" s) fs in
+
+    let _ = pf  "<!DOCTYPE html>
 <html>
 <head>
 <meta http-equiv=\"Content-Type\" content=\"text/html\"; charset=\"UTF-8\">
 <link href=\"song.css\" type=\"text/css\" rel=\"stylesheet\"/>
-<title>hello</title>
+<title>%s</title>
 </head>
 <body>
-" in
-    let pied_de_page = "</body></html>" in
-    let data = en_tete ^ (String.join "\n" data) ^ pied_de_page in
-      Std.output_file ~filename:Sys.argv.(2) ~text:data
+" title  in
+
+    List.iter ( fun c ->
+      match c with 
+      | Normal -> ()
+      | Titre s -> print_title pf s
+      | Grille g -> print_grille pf (g:string list)
+    ) data
+    ;
+    fprintf fout "</body></html>" ;
+    close_out fout
   with
     | e -> printf "ERREUR : %s\n" (Printexc.to_string e)
 
