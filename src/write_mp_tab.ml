dc79b09165f027751bedaea45b3d09185a8e99e4
laurent.carrie <laurent.carrie@gmail.com>
2016-09-19 14:26:22 +0200

    tab : separation ux et uy, dessin des noires et blanches

--- src/write_mp_tab.ml
+++ src/write_mp_tab.ml
@@ -11,15 +11,15 @@ let write_bar fout bar = (
   let pf fs = ksprintf ( fun s -> fprintf fout "%s" s) fs in
   let () = pf "
 %% write bar
-x1 := x0 + 2*u + %d*u ;
-draw(x0,base_line) -- (x0,base_line+5*1u) ;
+x1 := x0 + 2*ux + %d*ux ;
+draw(x0,base_line) -- (x0,base_line+5*1uy) ;
 " (* (List.length bar) *) 8  in
     
   let () = List.iter ( fun i ->
 pf "
-xii := x0 + %d*u ;
+xii := x0 + %d*ux ;
 pickup pencircle scaled 0.02bp ;
-%%draw(xii,base_line) -- (xii,base_line+5*1u) ;
+%%draw(xii,base_line) -- (xii,base_line+5*1uy) ;
 pickup pencircle scaled 0.15bp ;
 " i
   ) (List.init 4 ( fun i -> 2*i+1 ))
@@ -29,42 +29,60 @@ pickup pencircle scaled 0.15bp ;
     let notes = p.D.Tablature.notes in
     pf "
 %% position %d, %d notes
-xi := x0+%d*u ;
+xi := x0+%d*ux ;
 pickup pencircle scaled 0.02bp ;
-%%draw(xi,base_line) -- (xi,base_line+5*1u) ;
+%%draw(xi,base_line) -- (xi,base_line+5*1uy) ;
 pickup pencircle scaled 0.15bp ;
 " pos (List.length notes) pos ;
       List.iter ( fun n ->
       pf "
-  ynote := base_line+(6-%d)*u ;
+  ynote := base_line+(6-%d)*uy ;
   label(\"%d\" infont defaultfont scaled 0.6,(xi,ynote)) ;
-%%  draw(xi,ynote-u) -- (xi,base_line-u) ;
+%%  draw(xi,ynote-uy) -- (xi,base_line-uy) ;
 " n.D.Tablature.corde  n.D.Tablature.frette ;
       ) notes  ;
 
-      let (lowest_note: int ) = List.fold_left ( fun acc (n:D.Tablature.note) ->
-	if acc > n.D.Tablature.corde then acc else n.D.Tablature.corde
-      ) 1 notes in
+      let (lowest_note: int ) = 
+	match notes with
+	  | [] -> 6
+	  | hd::tl -> (
+	      List.fold_left ( fun acc (n:D.Tablature.note) ->
+		if acc > n.D.Tablature.corde then acc else n.D.Tablature.corde
+	      ) hd.D.Tablature.corde tl 
+	    )
+      in
 
-    let print_tail c = (
+      let print_tail c = (
       match p.D.Tablature.duration with
 	| 1 ->
-	    (*  croche *)
+	    (*  double croche *)
       pf "
-  ynote := base_line+(6-%d)*u ;
-  draw(xi,ynote-0.5*u) -- (xi,base_line-1*u) ;
-  draw(xi,base_line-1*u) -- (xi+0.5*u,base_line-1*u) ;
+  ynote := base_line+(6-%d)*uy ;
+  draw(xi,ynote-0.5*uy) -- (xi,base_line-1*uy) ;
+  draw(xi,base_line-1*uy) -- (xi+0.5*ux,base_line-0.8*uy) ;
+  draw(xi,base_line-0.6*uy) -- (xi+0.5*ux,base_line-0.4*uy) ;
 " c
 	| 2 ->
-	    (* noire *)
+	    (*  croche *)
       pf "
-  ynote := base_line+(6-%d)*u ;
-  draw(xi,ynote-0.5*u) -- (xi,base_line-1*u) ;
+  ynote := base_line+(6-%d)*uy ;
+  draw(xi,ynote-0.5*uy) -- (xi,base_line-1*uy) ;
+  draw(xi,base_line-1*uy) -- (xi+0.5*ux,base_line-0.8*uy) ;
 " c
 	| 4 ->
+	    (* noire *)
       pf "
-  ynote := base_line+(6-%d)*u ;
-  draw(xi,ynote-0.5*u) -- (xi,base_line-1*u) ;
+  ynote := base_line+(6-%d)*uy ;
+  draw(xi,ynote-0.5*uy) -- (xi,base_line-1*uy) ;
+  fill fullcircle scaled unote shifted (xi,base_line-1*uy) withcolor black ;
+" c
+	| 8 ->
+	    (* blanche *)
+      pf "
+  ynote := base_line+(6-%d)*uy ;
+  draw(xi,ynote-0.5*uy) -- (xi,base_line-1*uy) ;
+  fill fullcircle scaled unote shifted (xi,base_line-1*uy) withcolor white ;
+  draw fullcircle scaled unote shifted (xi,base_line-1*uy) withcolor black ;
 " c
 	| _ -> ()
 
@@ -75,7 +93,7 @@ pickup pencircle scaled 0.15bp ;
 
     let () = pf "%s" "
 for i=0 upto 5 :
-   draw (x0,base_line+i*1u) -- (x1,base_line+i*1u) ;
+   draw (x0,base_line+i*1uy) -- (x1,base_line+i*1uy) ;
   endfor
 x0:=x1 ;
 " in
@@ -91,8 +109,8 @@ let write_bar_line fout line = (
   let () = List.iter ( fun b -> write_bar fout b ) line in
 
   let () = pf "%s" "
-draw(x1,base_line) -- (x1,base_line+5*1u) ;
-base_line := base_line + 6*u ;
+draw(x1,base_line) -- (x1,base_line+5*1uy) ;
+base_line := base_line + 6*ux ;
 " in
     ()
 )
@@ -103,7 +121,9 @@ let  write_mp song name tab count  = (
   let pf fs = ksprintf ( fun s -> fprintf fout "%s" s) fs in
   let () = pf "%s" "
 beginfig(1) ;
-  u=0.2cm ;
+  uy=0.2cm ;
+  ux=0.25cm ;
+  unote=0.1cm ;
   b=10*u ;
   base_line=0*u ;
   line_offset=10*u ;
