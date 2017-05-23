0714e88d92c5ee92d9335ea0c02008d9529259ba
laurent.carrie <laurent.carrie@gmail.com>
2017-05-23 14:43:00 +0200

    je ne sais plus...

--- src/helpers.ml
+++ src/helpers.ml
@@ -1,6 +1,6 @@
 let (//) = Filename.concat
 
 let uppercase_compare s1 s2 =
-  String.compare (String.uppercase s1) (String.uppercase s2)
+  String.compare (String.uppercase_ascii s1) (String.uppercase_ascii s2)
 
 
