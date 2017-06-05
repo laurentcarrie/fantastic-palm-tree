a149afbcca92e28ec32d94e2a67f80fadb9b0caa
laurent.carrie <laurent.carrie@gmail.com>
2017-06-05 16:33:15 +0200

    positionnement accord dans la grille, avec boxit directement

--- src/write_mp_grille.ml
+++ src/write_mp_grille.ml
@@ -8,56 +8,13 @@ module D = Datamodel
 
 let (//) = Filename.concat
 
-let write_bar fout (bar:D.Grille.bar) = (
+let write_bar fout index (bar:D.Grille.bar) = (
   let pf fs = ksprintf ( fun s -> fprintf fout "%s" s) fs in
   let () = pf "%s" "
 %% write bar
 x1 := x0 + width ;
 draw(x0,base_line) -- (x0,base_line+height) ;
 " (* (List.length bar) *)   in
-
-
-(*
-  let e_of_chord c previous n nb = 
-    let idem = match previous with
-      | None -> false
-      | Some d -> d=c
-    in
-      if idem then (
-	sprintf "label.bot(btex %s etex,((x0+(x1-x0)*%d/%d,base_line+height*0.8))) ; \n" D.tex_idem n nb
-      ) else (
-	  match c.D.Accord.chord with
-	    | Some a -> 
-		let (a,sa,usa) = mp_of_chord a in
-		let rlap = sprintf "\\rlap{\\textsubscript{\\subscriptfont{%s}}}{\\textsuperscript{\\subscriptfont{%s}}}" sa usa in
-		  sprintf "label.bot(btex %s%s etex,(x0+(x1-x0)*%d/%d,base_line+height*2.5/3)) ; \n" a rlap n nb
-	    | None   -> 
-		sprintf "label.bot(btex %s etex,((x0+(x1-x0)*%d/%d,base_line+height*0.6))) ; \n" D.tex_silence n nb
-	)
-  in
-
-  let () = match bar.D.Grille.chords with
-    | [] -> (
-	pf "%%empty bar\n" ; 
-      )
-    | c::[] -> (
-	pf "%s" (e_of_chord c None 1  2)
-      )
-    | c1::c2::[] -> (
-	pf "%s" (e_of_chord c1 None 1  3) ;
-	pf "%s" (e_of_chord c2 (Some c1) 2  3)
-      )
-    | c1::c2::c3::c4::[] -> (
-	pf "%s" (e_of_chord c1 None 1  5) ;
-	pf "%s" (e_of_chord c2 (Some c1) 2  5) ;
-	pf "%s" (e_of_chord c3 (Some c2) 3  5) ;
-	pf "%s" (e_of_chord c4 (Some c3) 4  5)
-      )
-    | l -> (
-	pf "label(\"not managed %d\",((x1+x0)*2/3,base_line+2.5*uy)) ;\n" (List.length l) ;
-      )
-  in
-*)
 
   let (has_position:bool) = 
     match bar.D.Grille.chords with
@@ -74,28 +31,91 @@ draw(x0,base_line) -- (x0,base_line+height) ;
 	)
   in
 	    
-
-  let e_of_chord_with_position c nb = 
+  let e_of_chord_with_position pid i c nb = 
+    let bid = sprintf "%s_%d" pid i in
+    let ret = "" in
     match c.D.Accord.chord with
-      | Some a -> 
-	  let (a,sa,usa) = mp_of_chord a in
+      | Some a -> (
+	  let ret = sprintf "%s
+boxit.%s_a(btex \\notefont{%c} etex) ;
+boxit.%s_m(btex \\subscriptfont{%s} etex) ;
+" ret bid a.D.Accord.note bid (if a.D.Accord.minor then "m" else "") in 
+	  let (ret:string) = (if i mod 2 = 0 then 
+	    sprintf "%s
+%s_a.sw = %s.sw + 0.5 * (%s.nw-%s.sw) ;
+%s_a.ne = %s.ne ;
+%%fill bpath.%s_a withcolor (%s) ;
+"
+	      ret
+	      bid pid pid pid
+	      bid pid
+	      bid ".8,.2,.1"
+	    else
+	      sprintf "%s
+%s_a.sw = %s.sw ;
+%s_a.ne = %s.ne - 0.5 * (%s.ne-%s.se) ;
+"
+		ret
+		bid pid
+		bid pid pid pid
+	  )
+	  in
+	  let ret = sprintf "%s
+drawunboxed(%s_a) ;
+" 
+	    ret
+	    bid
+(*
 	  let rlap = sprintf "\\rlap{\\textsubscript{\\subscriptfont{%s}}}{\\textsuperscript{\\subscriptfont{%s}}}" sa usa in
-	    sprintf "label.bot(btex %s%s etex,(x0+(x1-x0)*%d/%d,base_line+height*2.5/3)) ; \n" a rlap (Option.get c.D.Accord.position) nb
+	    sprintf "(%s%s etex,((x1-x0)*%d/%d,height*2.5/3)) ; \n" a rlap (Option.get c.D.Accord.position) nb
+*)
+	  in
+	    (* majeur mineur *)
+	  let (ret:string) = 
+	    sprintf "%s
+%s_m.se = %s_a.se ;
+%s_m.nw = 0.2 * %s_a.nw + 0.8 * %s_a.se ;
+drawunboxed(%s_m) ;
+"
+	      ret
+	      bid bid
+	      bid bid bid
+	      bid
+	  in
+	    (* flat sharp *)
+	  let (ret:string) = 
+	    sprintf "%s
+boxit.%s_fs(btex %s etex) ;
+%s_fs.ne = %s_a.ne ;
+%s_fs.sw = 0.2 * %s_a.nw + 0.8 * %s_a.se ;
+drawunboxed(%s_fs) ;
+"
+	      ret
+	      bid (match a.D.Accord.alteration with | D.Accord.None -> "" | D.Accord.Flat ->  "\\flatsharpfont{$\\flat$}" | D.Accord.Sharp ->"\\flatsharpfont{$\\sharp$}" )
+	      bid bid
+	      bid bid bid
+	      bid
+	  in
+	    ret
+
+	)
       | None   -> 
-	  sprintf "label.bot(btex %s etex,((x0+(x1-x0)*%d/%d,base_line+height*0.6))) ; \n" D.tex_silence (Option.get c.D.Accord.position) nb
+	  sprintf "(%s etex,(((x1-x0)*%d/%d,height*0.6))) ; \n" D.tex_silence (Option.get c.D.Accord.position) nb
   in
 
+(*
   let e_of_chord_without_position c nb i = 
     match c.D.Accord.chord with
       | Some a -> 
 	  let (a,sa,usa) = mp_of_chord a in
 	  let rlap = sprintf "\\rlap{\\textsubscript{\\subscriptfont{%s}}}{\\textsuperscript{\\subscriptfont{%s}}}" sa usa in
-	    sprintf "label.bot(btex %s%s etex,(x0+(x1-x0)*%d/%d,base_line+height*2.5/3)) ; \n" a rlap i nb 
+	    sprintf "(%s%s etex,((x1-x0)*%d/%d,height*2.5/3)) ; \n" a rlap i nb 
       | None   -> 
-	  sprintf "label.bot(btex %s etex,((x0+(x1-x0)*%d/%d,base_line+height*0.6))) ; \n" D.tex_silence i nb 
+	  sprintf "(btex %s etex,(((x1-x0)*%d/%d,height*0.6))) ; \n" D.tex_silence i nb 
   in
+*)
 
-    
+(*
   let () = if has_position then (
     List.iter ( fun c ->
       pf "%s" (e_of_chord_with_position c 9)
@@ -107,37 +127,74 @@ draw(x0,base_line) -- (x0,base_line+height) ;
     ) bar.D.Grille.chords 
   )
   in
-    
+*)
 
 
-  let rec draw_petites_barres i b n =
-    if i=n then () else (
-      let (tb,ta) = if b then (4,0) else (1,0) in
+  let () = pf "
+%%   draw (x0,base_line) -- (x1,base_line) ;
+.
+%%   draw (x0,base_line+height) -- (x1,base_line+height) ;
+boxit.b%d(btex %s etex) ;
+b%d.sw=(x0,base_line) ;
+b%d.ne=(x1,base_line+height) ;
+%%fill bpath.b%d withcolor (.8,.2,.8) ;
+drawboxed(b%d) ;
+"
+index  
+""
+index index index index in
+  let () = if has_position then (
+    let nb = 10 in
+    List.iteri ( fun i c ->
+      let pos = Option.get c.D.Accord.position in
+      let (bid:string) = sprintf "b%d_%d" index i in
       pf "
-   xi:=x0+%d/%d*(x1-x0) ;
-   draw (xi,base_line-%d) -- (xi,base_line+%d) ;
-   draw (xi,base_line+height-%d) -- (xi,base_line+height+%d) ;
-" i n ta tb tb ta ;
-      draw_petites_barres (i+1) (not b) n 
-    )
-  in
-  let () = if has_position then draw_petites_barres 1 true 9 else () in
+%% has position
+boxit.%s(btex etex) ;
+%s.sw=(x0+(x1-x0)*(%d-1)/%d,base_line) ;
+%s.ne=(x0+(x1-x0)*(%d)/%d,base_line+height) ;
+drawunboxed(%s) ;
+%s
+" 
+	bid
+	bid (pos+1) nb
+	bid (pos+1) nb
+	bid
+	(e_of_chord_with_position bid i c nb)
+    ) bar.D.Grille.chords 
+  ) else (
+    (* let nb=9 in *)
+    List.iteri ( fun i c ->
+      pf "
+%% has no position
+boxit.b%d_%d(btex etex) ;
+b%d_%d.sw=(x0+(x1-x0)*%d/%d,base_line) ;
+b%d_%d.ne=(x0+(x1-x0)*(%d+1)/%d,base_line+height) ;
+drawboxed(b%d_%d) ;
+" 
+	index i
+(*	(e_of_chord_without_position c nb i)*)
+	index i i (List.length bar.D.Grille.chords)
+	index i i (List.length bar.D.Grille.chords)
+	index i
+    ) bar.D.Grille.chords
+  ) in
 
   let () = pf "%s" "
-   draw (x0,base_line) -- (x1,base_line) ;
-   draw (x0,base_line+height) -- (x1,base_line+height) ;
-   x0:=x1 ;
+x0:=x1 ;
 " in
     ()
 )
 
-let write_line fout (line:D.Grille.ligne) = (
+
+
+let write_line fout index (line:D.Grille.ligne) = (
   let pf fs = ksprintf ( fun s -> fprintf fout "%s" s) fs in
   let () = pf "%s" "
 %% write line
   x0:=0 ;
 "  in
-  let () = List.iter ( fun b -> write_bar fout b ) line.D.Grille.bars in
+  let () = List.iteri ( fun i b -> write_bar fout (index*100+i) b ) line.D.Grille.bars in
 
   let () = pf "%s" "
 draw(x1,base_line) -- (x1,base_line+height) ;
@@ -161,11 +218,13 @@ verbatimtex
 \\usepackage{latexsym}
 \\usepackage{nicefrac}
 \\usepackage{textcomp}
+\\newcommand*{\\notefont}{\\fontfamily{ptm}\\fontsize{10}{10}\\selectfont}
 \\newcommand*{\\subscriptfont}{\\fontfamily{ptm}\\fontsize{8}{8}\\fontshape{it}\\selectfont}
 \\newcommand*{\\flatsharpfont}{\\fontfamily{ptm}\\fontsize{8}{8}\\fontshape{it}\\selectfont}
 \\begin{document}
 etex
 
+input boxes ;
 beginfig(1) ;
   uy=0.2cm ;
 %%  ux=0.15cm ;
@@ -184,7 +243,7 @@ beginfig(1) ;
   in 
 
     
-  let () = List.iter ( fun l -> write_line fout l ) grille.D.Grille.lignes in
+  let () = List.iteri ( fun index l -> write_line fout index l ) grille.D.Grille.lignes in
  
 
     
